aa060e93ad67709f6abe76f72fae6f74824c3bd9
Nivored <mortenfjellaas@gmail.com>
2022-05-06 10:41:05 +0200

    update

--- Portfolio/Arbeid.js
+++ Portfolio/Arbeid.js
@@ -3,6 +3,9 @@ function myWork(){
     <div style="margin-left:50px;">In progress</div>
     <div style="margin-left:50px;"><a href="https://nivored.github.io/My-Work/Pokemon/"><img class="projekt" src="Prosjekt.png"></a></div>`;
 
+    html += `<div style="margin-left:50px;">In progress</div>
+    <div style="margin-left:50px;"><a  href="https://nivored.github.io/My-Work/Tibia/index.html"><img class="projekt" src="Tibia.png"</a></div>`
+    
     model.view = html;
     showApp();
 }
