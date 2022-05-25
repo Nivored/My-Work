db685fe22a0f146b7c32ec0ca5bc70d7af2aab43
Nivored <mortenfjellaas@gmail.com>
2022-05-25 18:25:54 +0200

    terminal page

---
+++ Terminal/commands.js
@@ -0,0 +1,58 @@
+var password = "mvc";
+var game = "https://nivored.github.io/My-Work/Game/index.html"
+var tibia = "https://nivored.github.io/My-Work/Tibia/index.html"
+
+whois = [
+    "<br>",
+    "Hey, I'm Morten!👋",
+    "Formaly worked as a mechanic",
+    "Now going to school at GET academy, learning to code",
+    "As of now i'm learning CSS, HMTL and JavaScript",
+    "I'm really in to gaming. FPS games, racing games, adventure RPG and other",
+    "<br>"
+  ];
+  
+  whoami = [
+    "<br>",
+    "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+    "Hard working. Willingness and eager to learn. Flexible when working with other people",
+    "I'm curious about how thing's work. No problem asking for help if i'm stuck or dont understand",
+    "<br>"
+  ];
+  
+  secret = [
+    "<br>",
+    '<span class="command">sudo</span>           Only use if you are strong',
+    "<br>"
+  ];
+  
+  projects = [
+    "<br>",
+    'game        <a href="' + game + '" target="_blank">Game, use W A S D to move' + "</a>",
+    'tibia       <a href="' + tibia + '" target="_blank">Tibia' + "</a>",
+    "<br>"
+  ];
+  
+  help = [
+    "<br>",
+    '<span class="command">whois</span>          Who is Morten?',
+    '<span class="command">whoami</span>         Who are you?',
+    '<span class="command">secret</span>         Find the password',
+    '<span class="command">projects</span>       View coding projects',
+    '<span class="command">history</span>        View command history',
+    '<span class="command">help</span>           You obviously already know what this does',
+    '<span class="command">clear</span>          Clear terminal',
+    '<span class="command">banner</span>         Display the header',
+    "<br>",
+  ];
+  
+  banner = [
+    '<span class="index">Morten.</span>',
+    '<span class="color2">Welcome to my little project.</span>',
+    "      __  _______  ____  _____________   __",
+    "     /  |/  / __ \\/ __ \\/_  __/ ____/ | / /",
+    "    / /|_/ / / / / /_/ / / / / __/ /  |/ / ",
+    "   / /  / / /_/ / _, _/ / / / /___/ /|  /  ",
+    "  /_/  /_/\\____/_/ |_| /_/ /_____/_/ |_/   ",
+    "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  ];
